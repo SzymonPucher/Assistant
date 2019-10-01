4326652ddf8fafd3498c1d160cf0e813d0039811
Szymon Pucher <sajmontube+gitlab@gmail.com>
2019-10-01 23:55:20 +0200

    Env change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "assistant-237304.firebaseapp.com",
+    databaseURL: "https://assistant-237304.firebaseio.com",
+    projectId: "assistant-237304",
+    storageBucket: "assistant-237304.appspot.com",
+    messagingSenderId: "869604397100",
+    appId: "1:869604397100:web:70555581642458b9"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "",
+    authDomain: "assistant-237304.firebaseapp.com",
+    databaseURL: "https://assistant-237304.firebaseio.com",
+    projectId: "assistant-237304",
+    storageBucket: "assistant-237304.appspot.com",
+    messagingSenderId: "869604397100",
+    appId: "1:869604397100:web:70555581642458b9"
+  }
 };
 
 /*
