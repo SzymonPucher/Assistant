0fdbeec02101066009fe8cab57638ae242dec52c
Szymon Pucher <sajmontube+gitlab@gmail.com>
2020-02-22 16:15:53 +0100

    Add enable details

--- src/app/budget/budget-expenses/budget-expenses.component.ts
+++ src/app/budget/budget-expenses/budget-expenses.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database'
 import { map } from 'rxjs/operators';
+import { MultivalueListComponent } from '../../common/multivalue-list/multivalue-list.component'
 
 
 @Component({
@@ -13,6 +14,8 @@ export class BudgetExpensesComponent implements OnInit {
 
   itemsRef: AngularFireList<any>;
   items: Observable<any[]>;
+
+  enableDetails: boolean;
   constructor(db: AngularFireDatabase) {
     this.itemsRef = db.list('expenses', ref => ref.orderByChild('Date'));
     // Use snapshotChanges().map() to store the key
@@ -20,9 +23,23 @@ export class BudgetExpensesComponent implements OnInit {
         changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
       )
     );
+    this.enableDetails = false;
   }
 
   ngOnInit() {
+  }
+
+  toggleDetails(){
+    this.enableDetails = !this.enableDetails;
+  }
+
+  showDetails(item){
+    var values = [];
+    Object.keys(item).forEach(elem => {
+      let val = item[elem] ? item[elem] : 'NULL';
+      values.push({label: elem, value: val});
+    });    
+    return values;
   }
 
   delete(key){
