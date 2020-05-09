ff35de4d6a7a1ab575808dde42142c6a0390c226
Szymon Pucher <sajmontube+gitlab@gmail.com>
2020-05-09 20:07:17 +0200

    Make app.module more readible

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireDatabaseModule } from "@angular/fire/database";
@@ -8,6 +7,9 @@ import { environment } from "../environments/environment";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+
+import { MenuComponent } from "./menu/menu.component";
+
 import { BudgetComponent } from "./budget/budget.component";
 import { BudgetExpensesComponent } from "./budget/budget-expenses/budget-expenses.component";
 import { BudgetExpensesAddComponent } from "./budget/budget-expenses/budget-expenses-add/budget-expenses-add.component";
@@ -17,29 +19,35 @@ import { BudgetLoansComponent } from "./budget/budget-loans/budget-loans.compone
 import { BudgetLoansAddComponent } from "./budget/budget-loans/budget-loans-add/budget-loans-add.component";
 import { BudgetInnertransferComponent } from "./budget/budget-innertransfer/budget-innertransfer.component";
 import { BudgetInnertransferAddComponent } from "./budget/budget-innertransfer/budget-innertransfer-add/budget-innertransfer-add.component";
-import { MenuComponent } from "./menu/menu.component";
+
 import { JournalComponent } from "./journal/journal.component";
-import { MultivalueListComponent } from "./shared/multivalue-list/multivalue-list.component";
 import { JournalEventComponent } from "./journal/journal-event/journal-event.component";
-import { UniversalSimpleFormComponent } from "./shared/universal-simple-form/universal-simple-form.component";
 import { JournalPomodoroComponent } from "./journal/journal-pomodoro/journal-pomodoro.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatMenuModule } from "@angular/material/menu";
+
+import { MultivalueListComponent } from "./shared/multivalue-list/multivalue-list.component";
+import { UniversalSimpleFormComponent } from "./shared/universal-simple-form/universal-simple-form.component";
+
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material";
-
-import { ChartsModule } from "ng2-charts";
+import { MatInputModule, MatMenuModule } from "@angular/material";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatIconModule } from "@angular/material/icon";
 import { MatSelectModule } from "@angular/material/select";
 import { MatDividerModule } from "@angular/material/divider";
 
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ChartsModule } from "ng2-charts";
+
+
 @NgModule({
   declarations: [
     AppComponent,
+
     MenuComponent,
+
     BudgetComponent,
     BudgetExpensesComponent,
     BudgetExpensesAddComponent,
@@ -49,31 +57,36 @@ import { MatDividerModule } from "@angular/material/divider";
     BudgetLoansAddComponent,
     BudgetInnertransferComponent,
     BudgetInnertransferAddComponent,
+
     JournalComponent,
-    MultivalueListComponent,
     JournalEventComponent,
-    UniversalSimpleFormComponent,
     JournalPomodoroComponent,
+
+    MultivalueListComponent,
+    UniversalSimpleFormComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    AppRoutingModule,
+
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireDatabaseModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
+
+    FormsModule,
+    ReactiveFormsModule,
+
     MatMenuModule,
     MatButtonModule,
     MatFormFieldModule,
-    ChartsModule,
     MatCardModule,
-    FormsModule,
     MatInputModule,
-    ReactiveFormsModule,
     MatProgressSpinnerModule,
     MatIconModule,
     MatSelectModule,
     MatDividerModule,
+
+    BrowserAnimationsModule,
+    ChartsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
